b8b49d26eb8ac48580568d75cc77e32acb4e3532
Randy Heiland <heiland@indiana.edu>
2019-09-12 15:00:50 -0400

    updates

--- custom_modules/embed.cpp
+++ custom_modules/embed.cpp
@@ -99,7 +99,6 @@ void create_cell_types( void )
 	cell_defaults.parameters.o2_reference = 38.0; 
 	
 	// set default uptake and secretion 
-	
 	static int oxygen_ID = microenvironment.find_density_index( "oxygen" ); // 0
 	
 	// oxygen 
@@ -109,20 +108,30 @@ void create_cell_types( void )
 
 	// set the default cell type to no phenotype updates 
 	// cell_defaults.functions.update_phenotype = tumor_cell_phenotype_with_oncoprotein; 
+
+	// not motile 
+	cell_defaults.phenotype.motility.is_motile = false; 
+
+	cell_defaults.phenotype.mechanics.cell_cell_adhesion_strength = 0.0;
+	cell_defaults.phenotype.mechanics.cell_cell_repulsion_strength = 5.0;
+
+	// add custom data 
+	// cell_defaults.custom_data.add_variable( "oncoprotein" , "dimensionless", 1.0 ); 
+
+
 	
 	//-------  cell type 0 ------------
 	embed_cell = cell_defaults;
-	embed_cell.name = "envelop cell"; 
+	embed_cell.name = "embed cell"; 
 	embed_cell.type = 0; 
+	embed_cell.phenotype.cycle.data.transition_rate( cycle_start_index , cycle_end_index ) = 0.0; 
+	embed_cell.phenotype.cycle.data.transition_rate( cycle_start_index , cycle_end_index ) = 0.01; 
 	
-	// add custom data 
-	
-	// cell_defaults.custom_data.add_variable( "oncoprotein" , "dimensionless", 1.0 ); 
-
 	//-------  cell type 1 ------------
 	env_cell = cell_defaults;
 	env_cell.name = "envelop cell"; 
 	env_cell.type = 1; 
+	env_cell.phenotype.cycle.data.transition_rate( cycle_start_index , cycle_end_index ) = 0.0; 
 	
 	return; 
 }
@@ -157,7 +166,7 @@ void setup_tissue( void )
 	// while ((infile >> x >> sep >> y >> sep >> z >> sep >> cell_type ) && (sep == ' '))
 	while (infile >> x >> y >> z >> cell_type ) 
 	{
-		std::cout << "cell type " << cell_type <<":  x,y= "<< x << "," << y ;
+//		std::cout << "cell type " << cell_type <<":  x,y= "<< x << "," << y ;
 
 		if (cell_type == 0)
 			pC = create_cell(embed_cell); 
@@ -165,132 +174,10 @@ void setup_tissue( void )
 			pC = create_cell(env_cell); 
 
 		pC->assign_position( x,y, 0.0 );
-		// pC->set_total_volume(volume);
-
-		// pC->phenotype.secretion.set_all_secretion_to_zero(); 
-		// pC->functions.update_phenotype = NULL; 
-		// pC->custom_data[ "time_of_death" ] = 0.0; 
-		
-		// if (idx == 0)
-		// 	pC->phenotype.molecular.internalized_total_substrates[ idxDeath ] = 19; 
-		// else
-		// 	pC->phenotype.molecular.internalized_total_substrates[ idxDeath ] = 0; 
 	}
 }
 
-
-void setup_tissue_hetero( void )
-{
-	// place a cluster of tumor cells at the center 
 	
-	double cell_radius = cell_defaults.phenotype.geometry.radius; 
-	double cell_spacing = 0.95 * 2.0 * cell_radius; 
-	
-	double tumor_radius = parameters.doubles( "tumor_radius" ); // 250.0; 
-	
-	// Parameter<double> temp; 
-	
-	int i = parameters.doubles.find_index( "tumor_radius" ); 
-	
-	Cell* pCell = NULL; 
-	
-	double x = 0.0; 
-	double x_outer = tumor_radius; 
-	double y = 0.0; 
-	
-	double p_mean = parameters.doubles( "oncoprotein_mean" ); 
-	double p_sd = parameters.doubles( "oncoprotein_sd" ); 
-	double p_min = parameters.doubles( "oncoprotein_min" ); 
-	double p_max = parameters.doubles( "oncoprotein_max" ); 
-	
-	int n = 0; 
-	while( y < tumor_radius )
-	{
-		x = 0.0; 
-		if( n % 2 == 1 )
-		{ x = 0.5*cell_spacing; }
-		x_outer = sqrt( tumor_radius*tumor_radius - y*y ); 
-		
-		while( x < x_outer )
-		{
-			pCell = create_cell(); // tumor cell 
-			pCell->assign_position( x , y , 0.0 );
-			pCell->custom_data[0] = NormalRandom( p_mean, p_sd );
-			if( pCell->custom_data[0] < p_min )
-			{ pCell->custom_data[0] = p_min; }
-			if( pCell->custom_data[0] > p_max )
-			{ pCell->custom_data[0] = p_max; }
-			
-			if( fabs( y ) > 0.01 )
-			{
-				pCell = create_cell(); // tumor cell 
-				pCell->assign_position( x , -y , 0.0 );
-				pCell->custom_data[0] = NormalRandom( p_mean, p_sd );
-				if( pCell->custom_data[0] < p_min )
-				{ pCell->custom_data[0] = p_min; }
-				if( pCell->custom_data[0] > p_max )
-				{ pCell->custom_data[0] = p_max; }				
-			}
-			
-			if( fabs( x ) > 0.01 )
-			{ 
-				pCell = create_cell(); // tumor cell 
-				pCell->assign_position( -x , y , 0.0 );
-				pCell->custom_data[0] = NormalRandom( p_mean, p_sd );
-				if( pCell->custom_data[0] < p_min )
-				{ pCell->custom_data[0] = p_min; }
-				if( pCell->custom_data[0] > p_max )
-				{ pCell->custom_data[0] = p_max; }
-		
-				if( fabs( y ) > 0.01 )
-				{
-					pCell = create_cell(); // tumor cell 
-					pCell->assign_position( -x , -y , 0.0 );
-					pCell->custom_data[0] = NormalRandom( p_mean, p_sd );
-					if( pCell->custom_data[0] < p_min )
-					{ pCell->custom_data[0] = p_min; }
-					if( pCell->custom_data[0] > p_max )
-					{ pCell->custom_data[0] = p_max; }
-				}
-			}
-			x += cell_spacing; 
-			
-		}
-		
-		y += cell_spacing * sqrt(3.0)/2.0; 
-		n++; 
-	}
-	
-	double sum = 0.0; 
-	double min = 9e9; 
-	double max = -9e9; 
-	for( int i=0; i < all_cells->size() ; i++ )
-	{
-		double r = (*all_cells)[i]->custom_data[0]; 
-		sum += r;
-		if( r < min )
-		{ min = r; } 
-		if( r > max )
-		{ max = r; }
-	}
-	double mean = sum / ( all_cells->size() + 1e-15 ); 
-	// compute standard deviation 
-	sum = 0.0; 
-	for( int i=0; i < all_cells->size(); i++ )
-	{
-		sum +=  ( (*all_cells)[i]->custom_data[0] - mean )*( (*all_cells)[i]->custom_data[0] - mean ); 
-	}
-	double standard_deviation = sqrt( sum / ( all_cells->size() - 1.0 + 1e-15 ) ); 
-	
-	std::cout << std::endl << "Oncoprotein summary: " << std::endl
-			  << "===================" << std::endl; 
-	std::cout << "mean: " << mean << std::endl; 
-	std::cout << "standard deviation: " << standard_deviation << std::endl; 
-	std::cout << "[min max]: [" << min << " " << max << "]" << std::endl << std::endl; 
-	
-	return; 
-}
-
 // custom cell phenotype function to scale immunostimulatory factor with hypoxia 
 void tumor_cell_phenotype_with_oncoprotein( Cell* pCell, Phenotype& phenotype, double dt )
 {
@@ -320,11 +207,13 @@ std::vector<std::string> my_coloring_function( Cell* pCell )
 	
 	if( pCell->type == 0 )  // embedded
 	{ 
-		output[0] = "black"; 
+		output[0] = "cyan"; 
+		output[2] = "gray"; 
 	} 
 	else if( pCell->type == 1 )  // enveloping
 	{ 
 		output[0] = "red"; 
+		output[2] = "gray"; 
 	} 
 	return output; 
 }
--- output/Readme.txt
+++ output/Readme.txt
@@ -1,4 +1,4 @@
-
+python anim_svg.py 1 0 0 2000
 python anim_svg.py 
 python anim_svg_substrate_step.py 0 0 0 2000 0 2000 -1000 1000 -1000 1000 0
 python anim_svg_substrate_grid_step.py 0 0 0 2000 0 2000 -1000 1000 -1000 1000 0
